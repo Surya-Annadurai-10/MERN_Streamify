ad94a394ca1e93ab4c50d427a5fc26c9c8cc4346
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-10-03 22:53:50 +0530

    bug fixed

--- Backend/index.js
+++ Backend/index.js
@@ -35,7 +35,7 @@ app.use("/api/connect", chatRouter);
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname , "../Frontend/dist")))
-  app.get("*" , (req , res)=>{
+  app.get("{0,}" , (req , res)=>{
     res.sendFile(path.join(__dirname , "../Frontend","dist","index.html"))
   });
 }
